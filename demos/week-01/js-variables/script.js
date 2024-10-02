71915691a1ba6696aa0b8bac99ee89c384a60c58
heinryanselm <acurakpa@st.ug.edu.gh>
2024-10-02 14:01:07 +0100

    JavaScript Variables and Datatypes

---
+++ demos/week-01/js-variables/script.js
@@ -0,0 +1,72 @@
+console.log("Hello World!!!");
+
+//let myName be equal to Heinry
+let myName1 = "Heinry";
+
+// console.log(myName);
+// console.log("myName")
+
+//Datatypes!
+
+//Strings
+//String is anything in ""
+let myName2 = "Heinry"
+let myString = "This is a random string"
+let myFavnumber = "62"
+
+//numbers
+let myAge = 62;
+let score = 102;
+let year = 2024
+let decimal = 1.2;
+
+console.log(myAge)
+console.log("myAge")
+
+// Boolean
+let timIsHandsome = true;
+let timHasHair = false;
+
+// let sum = myAge + decimal;
+// console.log(sum);
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+let sum = a + b;
+let difference = b - a;
+let quotient = b/a;
+let product = c * b;
+let isLessThan = a < b;
+let isGreaterThan = a > b;
+
+console.log(isLessThan);
+console.log(isGreaterThan);
+
+console.log(product)
+
+let str1 = 'Hello';
+let str2 = "World!";
+
+//concatenation
+let newString = str1 + str2;
+let newString2 = str1 + " " + str2;
+console.log(newString)
+
+//template literals
+let concString = `${str1} ${str2}`;
+console.log(concString);
+
+// typeof operator
+let name = 52;
+console.log(typeof name); //The output will be number
+
+let finalBoolean = true;
+console.log(`The type type of finalBoolean is ${typeof finalBoolean}`);
+
+let finalFinalString;
+console.log(finalFinalString)
+
+let finalNum = null;
+console.log(finalNum);
