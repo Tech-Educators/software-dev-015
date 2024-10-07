4b8631e4db0e3b17d931556363763da48489d75d
heinryanselm <acurakpa@st.ug.edu.gh>
2024-10-07 13:23:25 +0100

    Loops demo

---
+++ demos/week-02/loops/script.js
@@ -0,0 +1,22 @@
+console.log("Hello World!")
+
+//LOOPS
+// Two primary types of loops
+// for loops and while loops
+
+for (let i = 0; i < 20; i++){
+    console.log("FOR: ", i);
+}
+
+let counter = 0
+while (counter <= 10){
+    console.log("WHILE: ", counter);
+    counter++
+}
+
+//Loop through Arrays
+ let staff = ["Tim", "Heinry", "Jez", "Manny", "Joe", "Frankie", "Sam"];
+
+ for (let i = 0; i < staff.length; i++){
+    console.log("Staff: ", staff[i]);
+ }
