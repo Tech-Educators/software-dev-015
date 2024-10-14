3e48f1fb6dbe70ddf42b4002c7695c0f61738b3b
Tim <tim.smith2814@gmail.com>
2024-10-14 14:06:17 +0100

    Debugger demo

---
+++ demos/week-03/debugger/app.js
@@ -0,0 +1,30 @@
+const timsFavouriteNumber = 82;
+
+function countToTen() {
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+    debugger;
+  }
+}
+
+// countToTen();
+
+function add(a, b) {
+  debugger;
+  let sum = a + b;
+  console.log(sum);
+}
+
+// add(1, 4);
+
+function whileMyConsoleGentlyWeeps() {
+  debugger;
+  let count = 0;
+
+  while (count < 100) {
+    console.log(count);
+    count++;
+  }
+}
+
+whileMyConsoleGentlyWeeps();
