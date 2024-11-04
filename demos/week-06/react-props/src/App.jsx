b8531cf6f619641e776378c6e8190f694246ccd1
Tim <tim.smith2814@gmail.com>
2024-11-04 15:33:14 +0000

    Props demo

---
+++ demos/week-06/react-props/src/App.jsx
@@ -0,0 +1,10 @@
+import Grid from "./components/Grid";
+
+export default function App() {
+  return (
+    <>
+      <h1>Ooh a grid!</h1>
+      <Grid />
+    </>
+  );
+}
---
+++ demos/week-06/react-props/src/components/Square.jsx
@@ -0,0 +1,6 @@
+export default function Square({ num }) {
+  return <div className="square">{num}</div>;
+}
+
+// Square({ num })
+// this essentially means "Go and get me the num property from the object that is passed to Square"
