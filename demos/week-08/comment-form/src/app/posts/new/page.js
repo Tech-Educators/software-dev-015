ae9a516451ce7f8119e5e1a4896791b434ba3a75
Tim <tim.smith2814@gmail.com>
2024-11-20 14:47:48 +0000

    ASsignment skeleton

---
+++ demos/week-08/comment-form/src/app/posts/new/page.js
@@ -0,0 +1,11 @@
+export default function NewPostPage() {
+  return (
+    <div>
+      <h2>New Post</h2>
+      <p>
+        This page has a form that uses server actions to add a new post to the
+        page
+      </p>
+    </div>
+  );
+}
