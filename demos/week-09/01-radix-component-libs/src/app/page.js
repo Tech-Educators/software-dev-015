3736f86eed2a17574686040e7afd6dffb6f30b43
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-11-25 10:10:07 +0000

    demo

--- demos/week-09/01-radix-component-libs/src/app/page.js
+++ demos/week-09/01-radix-component-libs/src/app/page.js
@@ -1,10 +1,16 @@
 import PopoverDemo from "@/components/Popover";
 import ProgressDemo from "@/components/Progress";
+import { WiDayWindy } from "react-icons/wi";
+
 
 export default function Home() {
   return (
     <div>
+      <h2>Radix UI demos</h2>
+      <WiDayWindy/>
+      <h2>Progress bar</h2>
       <ProgressDemo/>
+      <p>Popover button</p>
       <PopoverDemo />
     </div>
   )
