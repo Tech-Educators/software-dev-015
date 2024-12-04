1ba7fe2f31b554da83a8ee6625f311f321cf1055
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-12-04 16:03:33 +0000

    add demo

---
+++ demos/week-10/app.js
@@ -0,0 +1,35 @@
+// part 1:
+
+// @arg n - how many layers you want of the triangle
+// @return - nothing
+// console.log the triangle to the console.
+function createRightTriangle(n) {
+
+}
+
+// for example 
+
+// n = 4
+// #
+// ##
+// ###
+// ####
+
+// part 2:
+
+
+// @args - n  - layers
+// @returns - nothing, just console.log
+
+function createPyramid(n) {
+    console.log('Hello')
+}
+
+createPyramid(4)
+
+// n = 2
+
+//    #
+//   ###
+//  #####
+// #######
